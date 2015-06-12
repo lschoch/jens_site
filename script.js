e42516fc17705ba32fcff6423ee35711c0e24cb6
Larry Schoch <lschoch@gmail.com>
2015-06-12 15:17:55 -0400

    Added some images, added footers, tweaked margins and layouts.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // alert("js opened");
+
+// function counter() {
+//     var i = 0
+
+//     return {
+//         count: function() {
+//             return i
+//         },
+//         increment: function() {
+//             i += 1
+//             return i
+//         },
+//         reset: function() {
+//             i = 0
+//             return i
+//         }
+//     }
+// }
+
+// var incrementer = counter()
+
+// console.log(incrementer.count())
+// console.log(incrementer.increment())
+// console.log(incrementer.increment())
+// console.log(incrementer.increment())
+// console.log(incrementer.reset())
