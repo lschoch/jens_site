3049fff284f5800f05b687e4f430a409a4aafa6d
Larry Schoch <lschoch@gmail.com>
2015-06-28 15:37:11 -0400

    Profile.php - replaced multiple lines of html code with header.php.

--- profile.php
+++ profile.php
@@ -1,34 +1,8 @@
-<?php  ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Dual Cred Eng: profile</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <script src="app.js"></script>
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-<body>
-
-<div class="container">
-    <header class="page-header">
-        <ul class="nav nav-pills pull-right">
-            <li><a href="index.php">Home</a></li>
-            <!-- <li><a href="profile.php">Profile</a></li> -->
-            <li><a href="calendar.php">Calendar</a></li>
-            <li><a href="resources.php">Resources</a></li>
-        </ul>
-        <h3>Dual Credit English</h3>
-    </header>
+<?php
+$title = "Dual Cred Eng: profile";
+$exclude = "profile";
+include("inc/header.php");
+?>
 
     <h4>Student Profile</h4>
     <br>
