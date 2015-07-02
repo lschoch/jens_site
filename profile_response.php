d650c4243cb93606512e2100d6d8e1bd0b2c779b
Larry Schoch <lschoch@gmail.com>
2015-07-02 09:54:30 -0400

    Created 'profile_response.php' to handle the redirect from 'profile_ process.php'. Deleted 'form_response.php', the file that was previously being used for this purpose. Created new data file, 'students.json'. Deleted the previous data file, 'profiles.txt'.

---
+++ profile_response.php
@@ -0,0 +1,14 @@
+<?php 
+$title = "Schoch Student Profile";
+$exclude = "profile";
+include("inc/header.php");
+?>
+
+<br><br>
+<div class="alert alert-success" id="profile_alert" role="alert"><h3>Thank you for updating your student profile!<h3>
+</div>
+<br><br><br>
+
+<?php
+include("inc/footer.php");
+?>
