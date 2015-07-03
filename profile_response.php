d839b40461bd772c6952035e59c4b851bee4faff
Larry Schoch <lschoch@gmail.com>
2015-07-02 23:33:18 -0400

    Added back button to profile_response.php with some associated changes to 'style.css'.

--- profile_response.php
+++ profile_response.php
@@ -5,10 +5,21 @@ include("inc/header.php");
 ?>
 
 <br><br>
+
 <div class="alert alert-success" id="profile_alert" role="alert"><h3>Thank you for updating your student profile!<h3>
+<br>
+<div class="form-group back">
+        <input type="button" id="back-btn" class="btn btn-info" value="Back" onclick="goBack()"></input>
+</div>
 </div>
 <br><br><br>
 
 <?php
 include("inc/footer.php");
 ?>
+
+<script>
+function goBack() {
+    window.history.back();
+}
+</script>
