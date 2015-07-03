bded68d271d72247c579b75e06da1cfc662de389
Larry Schoch <lschoch@gmail.com>
2015-07-02 23:26:22 -0400

    Modified profile_process.php - no longer appends to the data file. Instead it edits the record for the student_id and saves it back to the data file ('students.json').

--- profile_process.php
+++ profile_process.php
@@ -1,11 +1,31 @@
 <?php 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $file = "students.json";
-    $data = json_encode($_POST) . "\n";
-    file_put_contents($file, $data,  FILE_APPEND | LOCK_EX);
-    header("Location: profile_response.php");
-    exit;
-}
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $file = file_get_contents("students.json");
+        $stored = json_decode($file, true);
+        unset($file); // prevent memory leaks for large json
+    
+        // initialize any empty values
+        foreach($_POST as $key => $value) {
+            if ($_POST[$value] === "") {
+                $_POST[$value] = " ";
+            }
+        }
+
+        // search json file to find a record that matches the input student_id,  add form data to the record
+        foreach ($stored as $key => $value) { //looping through the student objects
+            foreach ($value as $k => $val) { //looping through the  values for an individual student
+                if ($k === "student_id" and $val === $_POST["student_id"])  { // look for a match to the input student_id
+                    $stored[$key] = $_POST; // save the $_post data to the $stored array
+                }
+            } 
+        }
+
+        file_put_contents('students.json', json_encode($stored));  //save the $stored array to the json file with json encoding
+        unset($stored); // release memory
+        header("Location: profile_response.php"); // redirect so that form won't be resubmitted
+        exit;
+    
+    }
 
 ?>
