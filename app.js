4bba91708e6a42402a2ce19e579a6b2015d26dba
Larry Schoch <lschoch@gmail.com>
2015-07-10 19:17:18 -0400

    Moved pertinenet statements into the if block that checks to for path = profile.php.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 		}
 	}
 
-	if($_path == '/jens_site/profile.php') {  // code specific to the profile.php document
+	if($_path == '/jens_site/profile.php') {  // code specific to the profile.php documents
 		formmodified=0; //reset formmodified when the document is loaded
 		startOver(); // reset profile_form and disable all buttons except student id
 
@@ -30,65 +30,62 @@ $(document).ready(function() {
 		window.onbeforeunload = confirmExit;
 		
 		$('#submit_btn').click(function() {
-			console.log('submit clicked');
 			formmodified = 0; // reset formmodified after the submit button is clicked (ie, changes have been sent, OK to navigate to another page without confirmExit)
 		});
+
+		$('#reset_btn').click(function() {
+			startOver(); // reset profile_form and disable all buttons except student id
+		});
+
+		//load data from students.json file corresponding to student_id
+		$('#student_id').change(function() {
+			$.getJSON( 'students.json', function( data ) {
+				var stud_id = $('#student_id').val(); // the value entered in the student_id input
+				var found = false;
+				$.each(data, function(index, value) {
+					if(value.student_id  == stud_id) {					
+						$('#first').val(value.first);
+				   		$('#last').val(value.last); 
+				   		$('#homtel').val(value.homtel);
+				   		$('#mobtel').val(value.mobtel);
+				   		$('#email').val(value.email);
+				   		$('#per1').val(value.per1);
+				   		$('#per2').val(value.per2);
+				   		$('#per3').val(value.per3);
+				   		$('#per4').val(value.per4);
+				   		$('#per5').val(value.per5);
+				   		$('#per6').val(value.per6);
+				   		$('#per7').val(value.per7);
+
+				   		found =true; // the input id was found in students.json, a valid id was entered
+
+				   		// reenable buttons, inputs, selects after input of valid id and populating form data from students.json file
+						$('input, select, button, submit').each(function(){
+								var $input = $(this);			   			
+								$input.removeAttr('disabled');
+						});
+				   		return(false);
+				   	}
+			   	} );
+			   	if(!found) {
+			   		alert("Sorry, that is not a valid Student ID. \nPlease recheck your Student ID and try again.");
+			   		$('#student_id').val(' ');
+			   		startOver(); // reset profile_form and disable all buttons except student id
+			   	} 
+			});
+		});
+		
+		//prevent submit on input keypresses 
+		$('input, selector, button').keypress(function(event){
+		        var enterOkClass =  $(this).attr('class');
+		        if (event.which == 13 && enterOkClass != 'enterSubmit' ) {  // add  'enterSubmit' class to any button for which submit on  enter should not be disabled
+		            event.preventDefault();
+		            $('input, select, button, submit').each(function(){
+	   			var prof_input = $(this);			   			
+	   			prof_input.blur();
+			});
+		            return false;   
+		        }
+	    	});
 	}
-
-	$('#reset_btn').click(function() {
-		console.log('reset clicked');
-		startOver(); // reset profile_form and disable all buttons except student id
-	});
-
-	//load data from students.json file corresponding to student_id
-	$('#student_id').change(function() {
-		$.getJSON( 'students.json', function( data ) {
-			var stud_id = $('#student_id').val(); // the value entered in the student_id input
-			var found = false;
-			$.each(data, function(index, value) {
-				if(value.student_id  == stud_id) {
-					
-					$('#first').val(value.first);
-			   		$('#last').val(value.last); 
-			   		$('#homtel').val(value.homtel);
-			   		$('#mobtel').val(value.mobtel);
-			   		$('#email').val(value.email);
-			   		$('#per1').val(value.per1);
-			   		$('#per2').val(value.per2);
-			   		$('#per3').val(value.per3);
-			   		$('#per4').val(value.per4);
-			   		$('#per5').val(value.per5);
-			   		$('#per6').val(value.per6);
-			   		$('#per7').val(value.per7);
-
-			   		found =true; // the input id was found in students.json, a valid id was entered
-
-			   		// reenable buttons, inputs, selects after input of valid id and populating form data from students.json file
-					$('input, select, button, submit').each(function(){
-							var $input = $(this);			   			
-							$input.removeAttr('disabled');
-					});
-			   		return(false);
-			   	}
-		   	} );
-		   	if(!found) {
-		   		alert("Sorry, that is not a valid Student ID. \nPlease recheck your Student ID and try again.");
-		   		$('#student_id').val(' ');
-		   		startOver(); // reset profile_form and disable all buttons except student id
-		   	} 
-		});
-	});
-	
-	//prevent submit on input keypresses 
-	$('input, selector, button').keypress(function(event){
-	        var enterOkClass =  $(this).attr('class');
-	        if (event.which == 13 && enterOkClass != 'enterSubmit' ) {  // add  'enterSubmit' class to any button for which submit on  enter should not be disabled
-	            event.preventDefault();
-	            $('input, select, button, submit').each(function(){
-   			var prof_input = $(this);			   			
-   			prof_input.blur();
-		});
-	            return false;   
-	        }
-    	});
 });
