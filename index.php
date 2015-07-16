4f93d9e235c7cc0e1f2342c83223e6fa3df9d59b
Larry Schoch <lschoch@gmail.com>
2015-07-16 11:56:39 -0400

    Added links for Google apps on index page.

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ $title = "SWHS Dual Credit English";
             </p>
             <h4>Google links:</h4>
             <p class="btn-group">
-                <a class="btn btn-primary btn-sm" href="#">Docs</a>
-                <a class="btn btn-success btn-sm" href="#" >Slides</a>
-                <a class="btn btn-default btn-sm" href="#" >Sheets</a>
-                <a class="btn btn-info btn-sm" href="#" >Drive</a>
+                <a class="btn btn-primary btn-sm" href="https://accounts.google.com/ServiceLogin?service=wise&passive=1209600&continue=https%3A%2F%2Fdocs.google.com%2Fdocument%2F%3Fusp%3Dmkt_docs%26pli%3D1&followup=https%3A%2F%2Fdocs.google.com%2Fdocument%2F%3Fusp%3Dmkt_docs%26pli%3D1&ltmpl=docs#identifier" target="_blank">Docs</a>
+                <a class="btn btn-success btn-sm" href="https://accounts.google.com/ServiceLogin?service=wise&passive=1209600&continue=https%3A%2F%2Fdocs.google.com%2Fpresentation%2F%3Fusp%3Dmkt_slides%26pli%3D1&followup=https%3A%2F%2Fdocs.google.com%2Fpresentation%2F%3Fusp%3Dmkt_slides%26pli%3D1&ltmpl=slides#identifier" target="_blank">Slides</a>
+                <a class="btn btn-default btn-sm" href="https://accounts.google.com/ServiceLogin?service=wise&passive=1209600&continue=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2F%3Fusp%3Dmkt_sheets%26pli%3D1&followup=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2F%3Fusp%3Dmkt_sheets%26pli%3D1&ltmpl=sheets#identifier" target="_blank">Sheets</a>
+                <a class="btn btn-info btn-sm" href="https://accounts.google.com/ServiceLogin?service=wise&passive=true&continue=http%3A%2F%2Fdrive.google.com%2F%3Futm_source%3Den%26utm_medium%3Dbutton%26utm_campaign%3Dweb%26utm_content%3Dgotodrive%26usp%3Dgtd%26ltmpl%3Ddrive&urp=https%3A%2F%2Fwww.google.com%2F#identifier" target="_blank">Drive</a>
             </p> 
         </div>
 
